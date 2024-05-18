127131d8610009a695ca5951719cebaf977d4036
João de Souza <joaogbdesouza@gmail.com>
2024-05-18 18:39:47 -0300

    loading env and db

--- Go/main.go
+++ Go/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/gabszero/recommendation-system-go/internals/infra"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	infra.InitDatabase()
 }
